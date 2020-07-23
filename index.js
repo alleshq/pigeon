8850b5d93d9476bfa98024229b6fdd508bb26179
Archie Baer <archie@abaer.dev>
2020-07-23 01:03:09 +0100

    Get email address

--- index.js
+++ index.js
@@ -2,19 +2,27 @@
 const express = require("express");
 const app = express();
 app.use(require("body-parser").json());
-app.use((err, req, res, next) => res.status(500).send("internalError"));
+app.use((err, req, res, next) => res.status(500).json({ err: "internalError" }));
 app.listen(8080);
 
 // Database
 const level = require("level");
 const db = level("./db", { valueEncoding: "json" });
 
+// Get Email Address
+app.get("/email/:id", (req, res) => {
+    db.get(req.params.id, (err, email) => {
+        if (err) return res.status(400).send({ err: "missingResource" });
+        res.json({ email });
+    });
+});
+
 // Set Email Address
 app.post("/email/:id", (req, res) => {
-    if (typeof req.body.email !== "string") return res.status(400).send("badRequest");
+    if (typeof req.body.email !== "string") return res.status(500).json({ err: "badRequest" });
 
     db.put(req.params.id, req.body.email, err => {
-        if (err) return res.status(500).send("internalError");
-        res.send("success");
+        if (err) return res.status(500).json({ err: "internalError" });
+        res.json({});
     });
 });
