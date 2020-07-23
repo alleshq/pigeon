5972a70e23ca590d39eb1a9ac6f11175c3021292
Archie Baer <archie@abaer.dev>
2020-07-23 01:17:42 +0100

    Send email

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+
 // Express
 const express = require("express");
 const app = express();
@@ -24,5 +26,38 @@ app.post("/email/:id", (req, res) => {
     db.put(req.params.id, req.body.email, err => {
         if (err) return res.status(500).json({ err: "internalError" });
         res.json({});
+    });
+});
+
+// Send Email
+app.post("/send/:id", (req, res) => {
+    if (
+        typeof req.body.subject !== "string" ||
+        typeof req.body.content !== "string"
+    ) return res.status(500).json({ err: "badRequest" });
+
+    // Get Address
+    db.get(req.params.id, (err, email) => {
+        if (err) return res.status(400).send({ err: "missingResource" });
+
+        // Send via Postal
+        axios.post(
+            "https://pico.alles.cx/api/v1/send/message",
+            {
+                to: [email],
+                from: "The Alles Robot <robot@alles.cx>",
+                subject: req.body.subject,
+                html_body: req.body.content
+            },
+            {
+                headers: {
+                    "X-Server-API-Key": process.env.POSTAL
+                }
+            }
+        ).then(() => {
+            res.json({});
+        }).catch(() => {
+            res.status(500).json({ err: "internalError" });
+        });
     });
 });
